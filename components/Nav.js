85cae1a12b53f8726ad41d2e535e7a3f08a061f2
Oki Ayobami <awhy14539@gmail.com>
2022-04-13 13:10:30 +0100

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -7,7 +7,7 @@ function Nav() {
       <h1>
         <img
           src="https://uploads-ssl.webflow.com/61f191256d6b9c6bb4d698ec/62069285acfd712e88b52676_Frame%209.png"
-          alt="logo"
+          alt="Logo"
         />
       </h1>
       <Link href="https://dapp.fragmynt.com">
